62149d8da0a557b04070f6a9209b3ef8c67c91e8
codesinging <codesinging@gmail.com>
2022-02-16 13:06:20 +0800

    新增基础文件: PinAdmin 应用类 Application

---
+++ src/Foundation/Application.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Foundation;
+
+class Application
+{
+    /**
+     * PinAdmin 版本号
+     *
+     * @var string
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * 返回 PinAdmin 版本号
+     *
+     * @return string
+     */
+    public function version(): string
+    {
+        return self::VERSION;
+    }
+}
