a03321d68040082977634dcc784d6a519c782bd2
codesinging <codesinging@gmail.com>
2022-02-16 13:14:09 +0800

    新增基础文件：PinAdmin 应用门面类 Admin.php

---
+++ src/Foundation/Admin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Foundation;
+
+use Illuminate\Support\Facades\Facade;
+
+class Admin extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return Application::LABEL;
+    }
+}
