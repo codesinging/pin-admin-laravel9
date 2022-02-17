d38f96fb721915b0823d3d82ab00b01dc70e94a9
codesinging <codesinging@gmail.com>
2022-02-17 23:37:44 +0800

    新增默认的首页和认证控制器

---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Controllers;
+
+use CodeSinging\PinAdmin\Foundation\Admin;
+use CodeSinging\PinAdmin\Support\Routing\Controller;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return Admin::name();
+    }
+}
