ebc6465142062eb9fba6219c134843597ff87016
codesinging <codesinging@gmail.com>
2022-02-17 21:39:17 +0800

    新增 Response 类

---
+++ src/Routing/Response.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Routing;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
+
+class Response
+{
+    /**
+     * 返回正确的 json 响应信息
+     *
+     * @param array|string|Collection|Model|null $message
+     * @param Model|array|Collection|null $data
+     *
+     * @return JsonResponse
+     */
+    public static function success(Model|array|string|Collection $message = null, Model|array|Collection $data = null): JsonResponse
+    {
+        $code = 0;
+        is_string($message) or list($data, $message) = [$message, $data];
+        return response()->json(compact('code', 'message', 'data'));
+    }
+
+    /**
+     * 返回错误的 json 响应信息
+     *
+     * @param string|null $message
+     * @param int $code
+     * @param mixed|null $data
+     *
+     * @return JsonResponse
+     */
+    public static function error(string $message = null, int $code = -1, mixed $data = null): JsonResponse
+    {
+        return response()->json(compact('message', 'code', 'data'));
+    }
+}
