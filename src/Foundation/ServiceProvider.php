470ce83805361e8930f4ee6499495fbb82c240ed
codesinging <codesinging@gmail.com>
2022-02-17 23:09:17 +0800

    服务提供者增加：注册数据库迁移目录，注册应用路由文件，注册中间件，注册视图命名空间，设置认证配置

--- src/Foundation/ServiceProvider.php
+++ src/Foundation/ServiceProvider.php
@@ -8,6 +8,10 @@ namespace CodeSinging\PinAdmin\Foundation;
 
 use CodeSinging\PinAdmin\Console\AdminCommand;
 use CodeSinging\PinAdmin\Console\ListCommand;
+use CodeSinging\PinAdmin\Middleware\Auth;
+use CodeSinging\PinAdmin\Middleware\Guest;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Config;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
@@ -20,6 +24,13 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         AdminCommand::class,
         ListCommand::class,
     ];
+
+    /**
+     * 所有应用
+     *
+     * @var Application[]
+     */
+    protected array $applications = [];
 
     /**
      * 注册 PinAdmin 服务
@@ -38,8 +49,33 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
+        $this->initApplications();
+
         if ($this->app->runningInConsole()) {
             $this->registerCommands();
+            $this->registerMigrations();
+        }
+
+        if (!$this->app->routesAreCached()) {
+            $this->registerRoutes();
+        }
+
+        $this->registerMiddlewares();
+        $this->registerViews();
+        $this->setAuthenticationConfig();
+    }
+
+    /**
+     * 初始化所有应用
+     *
+     * @return void
+     */
+    private function initApplications()
+    {
+        foreach (Admin::indexes() as $name => $options) {
+            if ($options['status']) {
+                $this->applications[$name] = new Application($name);
+            }
         }
     }
 
@@ -58,8 +94,72 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      *
      * @return void
      */
-    protected function registerCommands()
+    private function registerCommands()
     {
         $this->commands($this->commands);
+    }
+
+    /**
+     * 注册数据库迁移目录
+     *
+     * @return void
+     */
+    private function registerMigrations()
+    {
+        foreach ($this->applications as $application) {
+            $this->loadMigrationsFrom($application->path('migrations'));
+        }
+    }
+
+    /**
+     * 注册路由
+     *
+     * @return void
+     */
+    private function registerRoutes()
+    {
+        foreach ($this->applications as $application) {
+            $this->loadRoutesFrom($application->path('routes', 'web.php'));
+        }
+    }
+
+    /**
+     * 注册中间件
+     *
+     * @return void
+     */
+    private function registerMiddlewares()
+    {
+        /** @var Router $router */
+        $router = $this->app['router'];
+
+        $router->aliasMiddleware(Admin::label('auth', '.'), Auth::class);
+        $router->aliasMiddleware(Admin::label('guest', '.'), Guest::class);
+    }
+
+    /**
+     * 注册视图命名空间
+     *
+     * @return void
+     */
+    private function registerViews()
+    {
+        $this->loadViewsFrom(Admin::packagePath('resources', 'views'), Admin::label());
+
+        foreach ($this->applications as $application) {
+            $this->loadViewsFrom($application->path('views'), Admin::label($application->name(), '_'));
+        }
+    }
+
+    /**
+     * 设置应用认证守卫和提供者配置
+     * @return void
+     */
+    private function setAuthenticationConfig()
+    {
+        foreach ($this->applications as $application) {
+            Config::set('auth.guards.'. $application->guard(), $application->config('auth_guard'));
+            Config::set('auth.providers.'. $application->config('auth_guard.provider'), $application->config('auth_provider'));
+        }
     }
 }
