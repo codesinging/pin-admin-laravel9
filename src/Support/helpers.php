b64bbab048e6fd3fa8709064eaa3b5e114877305
codesinging <codesinging@gmail.com>
2022-02-19 15:32:25 +0800

    新增助手函数：admin_asset、admin_mix、success 和 error

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -5,9 +5,11 @@
  */
 
 use CodeSinging\PinAdmin\Foundation\Application;
+use CodeSinging\PinAdmin\Support\Routing\Response;
 use Illuminate\Config\Repository;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
 
 if (!function_exists('admin')) {
     /**
@@ -38,6 +40,35 @@ if (!function_exists('admin_config')) {
     function admin_config(array|string $key = null, mixed $default = null)
     {
         return admin()->config($key, $default);
+    }
+}
+
+if (!function_exists('admin_asset')) {
+    /**
+     * 返回 PinAdmin 应用静态文件路径
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    function admin_asset(string $path = ''): string
+    {
+        return admin()->asset($path);
+    }
+}
+
+if (!function_exists('admin_mix')) {
+    /**
+     * 返回带版本号的静态资源文件路径
+     *
+     * @param string $path
+     *
+     * @return string
+     * @throws Exception
+     */
+    function admin_mix(string $path): string
+    {
+        return admin()->mix($path);
     }
 }
 
@@ -82,5 +113,36 @@ if (!function_exists('admin_page')) {
     function admin_page(string $path): View|Factory
     {
         return admin()->page($path);
+    }
+}
+
+if (!function_exists('success')) {
+    /**
+     * 返回正确的 json 响应信息
+     *
+     * @param $message
+     * @param $data
+     *
+     * @return JsonResponse
+     */
+    function success($message = null, $data = null): JsonResponse
+    {
+        return Response::success($message, $data);
+    }
+}
+
+if (!function_exists('error')) {
+    /**
+     * 返回错误的 json 响应信息
+     *
+     * @param $message
+     * @param int $code
+     * @param $data
+     *
+     * @return JsonResponse
+     */
+    function error($message = null, int $code = -1, $data = null): JsonResponse
+    {
+        return Response::error($message, $code, $data);
     }
 }
