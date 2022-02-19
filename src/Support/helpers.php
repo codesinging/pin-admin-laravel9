1aae6cc319b21f7f600ed9e7243b4957ade52e5d
codesinging <codesinging@gmail.com>
2022-02-19 15:23:08 +0800

    新增助手函数：admin_template、admin_view 和 admin_page

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -6,6 +6,8 @@
 
 use CodeSinging\PinAdmin\Foundation\Application;
 use Illuminate\Config\Repository;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 if (!function_exists('admin')) {
     /**
@@ -36,5 +38,49 @@ if (!function_exists('admin_config')) {
     function admin_config(array|string $key = null, mixed $default = null)
     {
         return admin()->config($key, $default);
+    }
+}
+
+if (!function_exists('admin_template')) {
+    /**
+     * 返回位于 PinAdmin 应用目录的模板名
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    function admin_template(string $path): string
+    {
+        return admin()->template($path);
+    }
+}
+
+if (!function_exists('admin_view')) {
+    /**
+     * 返回位于 PinAdmin 应用目录的视图内容
+     *
+     * @param string|null $view
+     * @param array $data
+     * @param array $mergeData
+     *
+     * @return View|Factory
+     */
+    function admin_view(string $view = null, array $data = [], array $mergeData = []): View|Factory
+    {
+        return admin()->view($view, $data, $mergeData);
+    }
+}
+
+if (!function_exists('admin_page')) {
+    /**
+     * 返回位于 PinAdmin 应用目录内的单文件组件内容
+     *
+     * @param string $path
+     *
+     * @return View|Factory
+     */
+    function admin_page(string $path): View|Factory
+    {
+        return admin()->page($path);
     }
 }
