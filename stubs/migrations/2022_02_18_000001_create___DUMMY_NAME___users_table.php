abb8178d773c59050dc09dedd896fd56b04a0a47
codesinging <codesinging@gmail.com>
2022-03-05 10:46:21 +0800

    修改数据库迁移模板文件

--- stubs/migrations/2022_02_18_000001_create___DUMMY_NAME___users_table.php
+++ stubs/migrations/2022_02_18_000001_create___DUMMY_NAME___users_table.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Create__DUMMY_STUDLY_NAME__UsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -35,4 +35,4 @@ class Create__DUMMY_STUDLY_NAME__UsersTable extends Migration
     {
         Schema::dropIfExists('__DUMMY_NAME___users');
     }
-}
+};
--- stubs/migrations/2022_02_21_000002_create___DUMMY_NAME___menus_table.php
+++ stubs/migrations/2022_02_21_000002_create___DUMMY_NAME___menus_table.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Create__DUMMY_STUDLY_NAME__MenusTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -44,4 +44,4 @@ class Create__DUMMY_STUDLY_NAME__MenusTable extends Migration
     {
         Schema::dropIfExists('__DUMMY_NAME___menus');
     }
-}
+};
